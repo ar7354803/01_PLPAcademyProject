6b0d9ad69c8b3bcde95ba36953b10bda76b7ac15
Aziz Ur Rehman <134332782+ar7354803@users.noreply.github.com>
2025-09-02 17:11:39 +0500

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,15 @@
-
+print("Welcome to Python Calculator :")
+num1=int(input("Enter 1st Number: "))
+num2=int(input("Enter 2nd Number: "))
+print("+","-","*","/")
+operation=str(input("choose an operation: "))
+if operation=="+":
+    print(num1+num2)
+elif operation=="-":
+    print(num1-num2)
+elif operation=="*":
+    print(num1*num2)
+elif operation=="/":
+    print(num1/num2)
+else:
+    print("invalid operation please choose valid operation")
